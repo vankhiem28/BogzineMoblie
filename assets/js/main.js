65b1f85aca2ad479af0b63c79236d502612a47c6
vankhiem <vankhiem284@gmail.com>
2022-10-19 11:41:24 +0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(".featured__box").slick({
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+$(".topics__box").slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  speed: 300,
+  adaptiveHeight: true,
+});
+
+$(".podcasts__box").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 800,
+  adaptiveHeight: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
